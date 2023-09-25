0515c037722391ec401f2a1d277f42bf6ff1025e
souravgopal25 <souravgopal25@gmail.com>
2023-09-25 15:55:05 +0530

    Initial Commit

---
+++ src/main/java/com/tap/distributedlockingpoc/DistributedLockingPocApplication.java
@@ -0,0 +1,13 @@
+package com.tap.distributedlockingpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedLockingPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedLockingPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tap/distributedlockingpoc/DistributedLockingPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tap.distributedlockingpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedLockingPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
